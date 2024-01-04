4ead88b72f89a70b37e8c7bcb99af7e55ad5e322
hamed <hamed.bakhshi7809@gmail.com>
2024-01-04 14:09:19 +0100

    Made a new function where it takes a players selection and compares it to the computers choise. Depending on what they both chosed it will return the entity that won, or return a tie if they tied

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getComputerChoice(){
+    // Generates a random number between 0 and 2
+    // 0 = Rock
+    // 1 = paper
+    // scissor
 let num = Math.floor(Math.random() * 3)
 if(num == 0){
     return "rock"
@@ -9,4 +13,31 @@ else if(num == 1){
 return "scissor"
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    /* 
+    Get selection from player
+    Get selection from computer
+    Compare selections
+    return the winner with a string
+     */
+    let player = playerSelection.toLowerCase()
+    let computer = computerSelection
+    console.log(computer)
+    if( 
+        (player == "rock" && computer == "scissor")||
+        (player == "paper" && computer == "rock")||
+        (player == "sicssor" && computer == "paper") )
+        {
+       return 'Player Wins: ' + player + ' beats ' + computer + '.'
+    }
+    if( 
+        (computer == "rock" && player == "scissor")||
+        (computer == "paper" && player == "rock")||
+        (computer == "sicssor" && player == "paper") )
+        {
+       return 'Computer Wins: ' + computer + ' beats ' + player + '.'
+    }
+    return "Tie!"
+}
+console.log(playRound("Rock", getComputerChoice()))
+  
